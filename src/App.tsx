cce08eadd748f86a8ed63e7e7eb1a5146c7f4ed9
Hasan Üstün <hasan.ustun@std.yeditepe.edu.tr>
2022-09-24 20:24:37 +0300

    add tailwind to the project

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return <div className="">Hello World</div>;
+  return <div className="bg-red-200">Hello World</div>;
 }
 
 export default App;
