b93fbbdae19b3c3ca07d83b08ca10cccaf70e4d3
Hasan Üstün <hasan.ustun@std.yeditepe.edu.tr>
2022-09-25 02:44:13 +0300

    add color defs

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,11 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'header-blue': '#1EA4CE',
+        primary: '#1EA4CE',
         'header-dark-blue': '#147594',
         'c-gray-500': '#697488',
-        'c-black-500': '#525252',
+        'c-black-600': '#525252',
+        'c-black-500': '#6F6F6F',
       },
       boxShadow: {
         'filter-item': '0px 1px 7px rgba(93, 56, 192, 0.4)',
