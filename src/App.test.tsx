9d01b1edf9acb3d29e710d7a55ee2ec5098cf2ef
Hasan Üstün <hasan.ustun@std.yeditepe.edu.tr>
2022-09-28 16:07:15 +0300

    update app component tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders product text', () => {
   render(
     <Provider store={store}>
       <App />
@@ -12,4 +12,34 @@ test('renders learn react link', () => {
   );
 
   expect(screen.getByText(/product/i)).toBeInTheDocument();
+});
+
+test('add product button appears', async () => {
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+
+  const addButton = await screen.findAllByText('Add', undefined, {
+    timeout: 3000,
+  });
+  expect(addButton[0]).toBeInTheDocument();
+});
+
+test('changes price after adding product', async () => {
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+
+  const addButton = await screen.findAllByTestId('testbutton', undefined, {
+    timeout: 3000,
+  });
+
+  fireEvent.click(addButton[0]);
+
+  const price = screen.queryByText(/0.00/i);
+  expect(price).not.toBeInTheDocument();
 });
